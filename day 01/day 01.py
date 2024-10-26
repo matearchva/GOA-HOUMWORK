ece89d0cd16c25cbf47b14c740c15ac231758b36
matearchva <gujabidzetsotne4@gmail.com>
2024-10-26 06:44:59 -0700

    commit

---
+++ day 01/day 01.py
@@ -0,0 +1,16 @@
+name = "mate"  #სტრინგი
+
+
+print(name)
+
+surname="archzadze"  #სტრინგი
+print(surname)
+age=13  
+height=1.55   #floot
+
+
+print(height)
+print(age)
+
+
+print(name, surname, age, height,)
