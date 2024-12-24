57aa1f9eb970b7971baa02137d80a6816583e38c
matearchva <gujabidzetsotne4@gmail.com>
2024-12-24 07:50:31 -0800

    commit

---
+++ day09/hv.py
@@ -0,0 +1,37 @@
+#name = input("Enter your name> ")
+
+#age = int(input("Enter your age> "))
+
+#print("გამარჯობა მე ვარ", name, "შენ ხარ", age, "წლის")
+
+
+
+
+num =int(input("Enter your num> "))
+
+symbol =(input("Enter your symbol> "))
+
+num2 =int(input("Enter your num> "))
+
+
+print ("5")
+
+
+
+
+
+
+
+
+
+
+
+
+equation= int(input ("20+50=70"))
+
+ 
+print("equation")
+
+
+
+
---
+++ day09/kw.py
@@ -0,0 +1,29 @@
+name =input("mate: ")
+surname =input ("arcva: ")
+age =input ("13: ")
+nam1 =input("20: ")
+nam2 =input("80: ")
+
+
+
+
+print(name)
+print(surname)
+print(age)
+print(nam1)
+print(nam2)
+
+
+nam3 =20
+nam4 =30
+
+
+print(nam3+nam4)
+
+
+
+
+
+
+
+
