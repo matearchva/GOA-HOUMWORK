f46e66d22894300e11c496be04122aa1c152a366
matearchva <gujabidzetsotne4@gmail.com>
2025-01-28 12:06:38 -0800

    commit

---
+++ day10/homework/classvork.py
@@ -0,0 +1,31 @@
+
+
+name = input("შეიყვანეთ თქვენი სახელი: ")
+age = int(input("შეიყვანეთ თქვენი ასაკი: "))
+print(f"გამარჯობა, {name}! შენ ხარ {age} წლის.")
+
+
+
+family_member1 = input("არჩილი: ")
+age1 = int(input(f"{family_member1} -47  "))
+family_member2 = input("ნინო: ")
+age2 = int(input(f"{family_member2} - 45 "))
+
+
+print(f"{family_member1} 10 წელიწადში იქნება {age1 + 10} წლის.")
+print(f"{family_member2} 10 წელიწადში იქნება {age2 + 10} წლის.")
+
+
+
+
+
+
+
+num1 = float(input("8: "))
+num2 = float(input("2: "))
+
+
+print(f"+: {num1 + num2}")
+print(f"-: {num1 - num2}")
+print(f"*: {num1 * num2}")
+print(f"/: {num1 / num2}")
---
+++ day15/clasvork/clasvork.py
@@ -0,0 +1,26 @@
+
+
+print(1>1 and 1<1)#false
+print(1>2 and 0<1)#false
+print(2>1 and 2<3)#true
+print(3>1 and 4>1)#true
+
+
+
+print(2>1 or 2>1)#true
+print(3<1 or 2>3)#false
+print(3>4 or 1>2)#false
+print(1>1 or 1>1)#false
+
+
+
+
+
+
+
+
+
+
+
+
+
